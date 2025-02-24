697ac2df66367c9e1febe444c9084986a7665b9d
tchen <chen2015tong@gmail.com>
2025-02-24 16:53:06 +0100

    refresh database design

--- relational-databases/oracle/src/main/script/BaseScript.sql
+++ relational-databases/oracle/src/main/script/BaseScript.sql
@@ -8,13 +8,16 @@ CREATE TABLE T_BATCHING_COMMENT (
 -- call procedure_name ('', '') into ..
 
 -- 查找用户自定义的对象或者是方法
->SELECT * FROM user_procedures
->select * from table where rownum <= 100; -- 返回行数在前100的数据
->select * from record where NVL(col1, 'NULL') -- Oracle空字段值的判断
+SELECT * FROM user_procedures
+select * from table where rownum <= 100; -- 返回行数在前100的数据
+select * from record where NVL(col1, 'NULL') -- Oracle空字段值的判断
 
->select * from USER_OBJECTS
->select count(1) from USER_OBJECTS where OBJECT_TYPE = 'FUNCTION' and OBJECT_NAME = 'Procedure_name'
+select * from USER_OBJECTS
+select count(1) from USER_OBJECTS where OBJECT_TYPE = 'FUNCTION' and OBJECT_NAME = 'Procedure_name'
 
->select table_name from all_tables where table_name = '%s'; -- 判断指定的表名称是否存在
+select table_name from all_tables where table_name = '%s'; -- 判断指定的表名称是否存在
 
->select value from v$sysstat where name='user commits' -- 统计指定时间段内的提交量
+select value from v$sysstat where name='user commits' -- 统计指定时间段内的提交量
+
+-- TODO. 对特殊字符进行转义后存储
+INSERT INTO customer (id, customer_name) VALUES (11, 'ABC'' Company');
