3771cda147b77dd17eecaed6d7077e08b030d8da
chent5 <chen2015tong@gmail.com>
2025-05-21 18:40:00 +0200

    update demo sql server jdbc

--- relational-databases/sql-server/src/main/java/DemoSqlServerJDBC.java
+++ relational-databases/sql-server/src/main/java/DemoSqlServerJDBC.java
@@ -4,19 +4,28 @@ import java.sql.*;
 public class DemoSqlServerJDBC {
 
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:sqlserver://server:1433;instanceName=MSSQLSERVER;databaseName=QA_FMM412_WJI";
-        Connection connection = DriverManager.getConnection(url, "Q_WJI", "Hello00");
-        String query = "create table test(id_num int, username nvarchar(100));";
+        String url = "jdbc:sqlserver://127.0.0.1:1522;databaseName=FUM;trustServerCertificate=true";
+        Connection connection = DriverManager.getConnection(url, "sa", "Hello00123");
 
+        String sql = "select * from dbo.ADM_ACCESS_TOKEN";
         Statement statement = connection.createStatement();
-        statement.executeUpdate(query);
-
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            System.out.println(id);
+        }
         connection.close();
         System.out.println("Finish");
     }
 
-    // TODO. PreparedStatement替换的参数实际值需要满足类型匹配
-    public static void testMain() throws SQLException {
+    private static void testCreateTable(Connection connection) throws SQLException {
+        String query = "create table test(id_num int, username nvarchar(100));";
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+
+    // TODO. PreparedStatement 参数的实际值需要满足类型匹配
+    public static void testInsertTable() throws SQLException {
         String url = "jdbc:sqlserver://server:1433;databaseName=DAS_CONV_TOOL;trustServerCertificate=true";
         Connection connection = DriverManager.getConnection(url, "INSTAL", "INSTALL");
         String query = "insert into test_table (code, value1, value2) values (?, ?, ?)";
